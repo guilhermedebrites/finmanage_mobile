27f4b67a23e83692b27c52eecef8c9f153563964
guilhermedebrites <guilhermedebrites1@gmail.com>
2024-09-06 01:01:30 -0300

    feat: Start login screen and integration with firebase

---
+++ lib/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
